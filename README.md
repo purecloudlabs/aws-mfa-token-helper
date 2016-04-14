95db2ae09e0f0dd1657fa178dcaa6ca9e4f7586e
Xander Dumaine <xander.dumaine@inin.com>
2016-04-14 09:49:43 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+Welcome!
+
+If you're reading this, you probably don't have any idea how to set up your
+AWS credentials. Fortunately, with the powers of computers, we're goint to make
+this easy for you!
+
+Assumptions
+1. You can `npm install` stuff
+2. You have AWS credentials for the env you need and you've set up an MFA device
+
+Ready? Go.
+
+1. `npm install` - you've used node before, right?
+2. Get your AWS credentials. You could technically do this first, if you wanted,
+but this is an exercise in following directions, so pay attention.
+    1. Log into the aws console
+    2. Go to Security Credentials (under your account
+        at the top right)
+    3. Search your name
+    4. Click your name
+    5. Create an access key
+    6. Copy the template creds file to your aws home -
+    `cp credentials ~/.aws/credentials`
+    7. Copy the access key id and secret access key into that file
+    8. Copy the mfa file to your aws home -
+    `cp mfa_id ~/.aws/mfa_id`
+    8. Copy your MFA token into that file.
+    9. Think, "wouldn't it have been easier to just
+    `echo <mfa:device:id> ~/.aws/mfa_id`"
+    10. Remember what I said about following directions and just do it
+    11. npm start
+    12. Enter your MFA token
+    13. Check `~/.aws/credentials` to make sure you have a `[default]` section
+    with credentials
+    14. Send Matt Cheely and Xander Dumaine $5
---
+++ aws-auth.js
@@ -0,0 +1,95 @@
+#!/usr/bin/env node
+'use strict';
+
+var AWS = require('aws-sdk');
+var cp = require('child_process');
+var fs = require('fs');
+var ini = require('ini');
+var path = require('path');
+var bluebird = require('bluebird');
+var promisify = bluebird.promisify;
+var prompt = require('prompt');
+
+var CREDENTIALS_FILE = path.join(process.env.HOME, '.aws/credentials');
+var MFA_ID_FILE = path.join(process.env.HOME, '.aws/mfa_id');
+var TOKEN_LIFETIME_SECONDS = 129600; //36hr
+
+var credentialConfig = ini.parse(fs.readFileSync(CREDENTIALS_FILE, 'utf-8'));
+
+/* Do the things */
+authenticateAws()
+    .then(runCommandArgs);
+
+
+function authenticateAws() {
+    console.log('Checking cached credentials...');
+    if (credentialsExpired()) {
+        return promptMfaToken()
+                .then(getCredentials)
+                .then(storeCredentials)
+                .then(function () {
+                    console.log('Credentials Updated.');
+                });
+
+    } else {
+        console.log('Credentials are still good.');
+        return bluebird.resolve();
+    }
+}
+
+function credentialsExpired() {
+    var expiration = credentialConfig.default && credentialConfig.default.expiration;
+    if (!expiration) {
+        console.log('No credentials found');
+        return true;
+    } else if (new Date(expiration) < new Date()) {
+        console.log('Credentials expired on ' + expiration);
+        return true;
+    } else {
+       return false;
+    }
+}
+
+function getCredentials(mfaKey) {
+    AWS.config.credentials = new AWS.SharedIniFileCredentials({
+        profile: 'master'
+    });
+    var sts = new AWS.STS();
+    var mfaIdentifier = loadMfaIdentifier();
+    return promisify(sts.getSessionToken.bind(sts))({
+        DurationSeconds: TOKEN_LIFETIME_SECONDS,
+        SerialNumber: mfaIdentifier,
+        TokenCode: mfaKey,
+    }).then(function (response) {
+        return response.Credentials;
+    });
+}
+
+function loadMfaIdentifier() {
+    return fs.readFileSync(MFA_ID_FILE, 'utf-8').trim();
+}
+
+function promptMfaToken() {
+    prompt.start();
+    return promisify(prompt.get)('AWS MFA token')
+        .then(function(result) {
+            return result['AWS MFA token'];
+        });
+}
+
+function runCommandArgs() {
+    var cmd = process.argv.slice(2);
+    cp.spawnSync(process.argv[2], process.argv.slice(3), {
+        stdio: [0, 0, 0]
+    });
+}
+
+function storeCredentials(credentials) {
+    credentialConfig.default = {};
+    credentialConfig.default.aws_access_key_id = credentials.AccessKeyId;
+    credentialConfig.default.aws_secret_access_key = credentials.SecretAccessKey;
+    credentialConfig.default.aws_session_token = credentials.SessionToken;
+    credentialConfig.default.expiration = credentials.Expiration.toString();
+    fs.writeFileSync(CREDENTIALS_FILE, ini.stringify(credentialConfig));
+    return credentials;
+}
